abd2fd6ec38808f76dfde5d966ef6bfa43393a2f
mrwilbroad <wfrancis169@gmail.com>
2024-06-29 00:59:07 +0300

    Initialization

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className="container">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe vitae, possimus quis amet et fugit officiis omnis enim molestias laudantium corrupti reiciendis fugiat laborum dolorem nostrum optio assumenda tenetur voluptatum.
+    </main>
+  );
+}
