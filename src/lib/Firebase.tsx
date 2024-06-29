f9419ab24415ae7184bd31a24136b6a143f337f1
mrwilbroad <wfrancis169@gmail.com>
2024-06-29 14:28:24 +0300

    env var

--- src/lib/Firebase.tsx
+++ src/lib/Firebase.tsx
@@ -2,12 +2,12 @@ import { initializeApp, type FirebaseOptions } from 'firebase/app'
 import { getFirestore }  from "firebase/firestore";
 
 const firebaseConfig : FirebaseOptions = {
-    apiKey: "AIzaSyDzTP9QF6FvP7S4fnbK1h99s2aeqAI6Ju8",
-    authDomain: "nextjs-blogger-7126f.firebaseapp.com",
-    projectId: "nextjs-blogger-7126f",
-    storageBucket: "nextjs-blogger-7126f.appspot.com",
-    messagingSenderId: "1021736846471",
-    appId: "1:1021736846471:web:2177c08d6134f5d82ce422"
+    apiKey: process.env.NEXT_PUBLIC_apiKey,
+    authDomain: process.env.NEXT_PUBLIC_authDomain,
+    projectId: process.env.NEXT_PUBLIC_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_appId
 }
 
 // Initialize Firebase
